0f25fcd87aa87a77ae322b34256ac0e9c36ac673
Boyuan Cui <boyuanbryan.cui@mail.utoronto.ca>
2025-02-18 22:05:46 -0500

    Copied Picotron train scripts to here

--- picotron/utils.py
+++ picotron/utils.py
@@ -109,7 +109,7 @@ def download_model(model_name, hf_token):
     print("Downloading SafeTensors files...")
     huggingface_hub.snapshot_download(model_name, repo_type="model", local_dir="hf_model_safetensors", token=hf_token,
                                       allow_patterns=["*.safetensors", "*.json"])
-    # Check if the model has SafeTensors files
-    if not os.path.exists("hf_model_safetensors/model.safetensors"):
-        raise ValueError(f"Model {model_name} does not have SafeTensors files.")
+    # # Check if the model has SafeTensors files
+    # if not os.path.exists("hf_model_safetensors/model.safetensors"):
+    #     raise ValueError(f"Model {model_name} does not have SafeTensors files.")
     print("SafeTensors files downloaded successfully! ✅")
